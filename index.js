2cf9ace7d135ad4ed6ac1bff8bce701cf0f9ec9a
Abdulraheem Ahmad <abdulahmad586@gmail.com>
2023-06-10 15:36:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
+
+app.get("/api/", function (req, res) {
+  const time = new Date()
+   res.json({unix: time.getTime(), utc: time.toUTCString()});
+});
+
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
